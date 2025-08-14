aa2a4d09e0899db9fe8add215e1492a2d0d76a52
JCalago02 <jcalago02@gmail.com>
2025-08-14 01:28:43 -0500

    POC for monthly to weekly data upsampling

--- utils/custom_types.py
+++ utils/custom_types.py
@@ -11,12 +11,56 @@ class FuelType(Enum):
     OTHER = "OTH"
 
 class Respondent(Enum):
-    PJM = "PJM"
-    MISO = "MISO"
-    ERCOT = "ERCO"
+    CAL = "CAL"
+    NORTHWEST = "NW"
+    SOUTHWEST = "SW"
+    TEXAS = "TX"
+    CENTRAL = "CENT"
+    MIDWEST = "MIDW"
+    TENNESSEE = "TEN"
+    FLORIDA = "FL"
+    SOUTHEAST = "SE"
+    CAROLINAS = "CAR"
+    MIDATLANTIC = "MIDA"
+    NEWYORK = "NY"
+    NEWENGLAND = "NE"
 
 class Timezone(Enum):
     EASTERN = "Eastern"
     CENTRAL = "Central"
     MOUNTAIN = "Mountain"
     PACIFIC = "Pacific"
+
+class StorageRegion(Enum):
+    EAST = "NW2_EPG0_SWO_R31_BCF"
+    MIDWEST = "NW2_EPG0_SWO_R32_BCF"
+    PACIFIC = "NW2_EPG0_SWO_R35_BCF"
+    SOUTH = "NW2_EPG0_SWO_R33_BCF"
+    MOUNTAIN = "NW2_EPG0_SWO_R34_BCF"
+    SALT = "NW2_EPG0_SSO_R33_BCF"
+    NONSALT = "NW2_EPG0_SNO_R33_BCF"
+    LOWER_48 = "NW2_EPG0_SWO_R48_BCF"
+
+class EIAConsumptionType(Enum):
+    ELECTRICITY = "VEU"
+    COMMERCIAL = "VCS"
+    VEHICLEFUEL = "VDV"
+    DELIVERY = "VGT"
+    INDUSTRIAL = "VIN"
+    RESIDENTIAL = "VRS"
+
+storage_region_to_noaa_states = {
+    StorageRegion.EAST: ["FL", "GA", "SC", "NC", "VA", "WV", "OH", "PA", "MD", "DE", "NJ", "NY", "CT", "RI", "MA", "NH", "ME", "VT"],
+    StorageRegion.MIDWEST: ["MN", "IA", "MO", "IL", "WI", "MI", "IN", "KY", "TN"],
+    StorageRegion.SOUTH: ["TX", "OK", "KS", "AR", "LA", "MS", "AL"],
+    StorageRegion.MOUNTAIN: ["NV", "AZ", "UT", "ID", "MT", "WY", "CO", "NM", "NE", "SD", "ND"],
+    StorageRegion.PACIFIC: ["CA", "OR", "WA"]
+}
+
+storage_region_to_power_gen_respondent_region = {
+    StorageRegion.EAST: [Respondent.MIDATLANTIC, Respondent.NEWYORK, Respondent.NEWENGLAND, Respondent.CAROLINAS, Respondent.FLORIDA],
+    StorageRegion.MIDWEST: [Respondent.MIDWEST, Respondent.TENNESSEE],
+    StorageRegion.SOUTH: [Respondent.TEXAS, Respondent.SOUTHEAST],
+    StorageRegion.MOUNTAIN: [Respondent.CENTRAL, Respondent.NORTHWEST, Respondent.SOUTHWEST],
+    StorageRegion.PACIFIC: [Respondent.CAL, Respondent.NORTHWEST]
+}
--- utils/eia_api.py
+++ utils/eia_api.py
@@ -4,15 +4,26 @@ import requests
 import json
 from typing import Dict, List, Tuple
 from datetime import datetime
-from utils.custom_types import FuelType, Respondent, Timezone
+from utils.custom_types import (
+    FuelType, 
+    Respondent, 
+    Timezone, 
+    StorageRegion, 
+    EIAConsumptionType,
+    storage_region_to_power_gen_respondent_region
+)
 from typing import Any, Optional
+import pandas as pd
 
 MAX_QUERY_SIZE = 5000
 SET_TIMEZONE = Timezone.EASTERN # Hardcoded to ensure all data queries are consistent
 
 class EIADataPuller:
-    def __init__(self):
+    def __init__(self, storage_region: StorageRegion):
         self.api_key = os.getenv('EIA_API_KEY')
+        self.storage_region: StorageRegion = storage_region
+        self.power_gen_respondents: List[Respondent] = storage_region_to_power_gen_respondent_region[storage_region]
+        self.eia_duoareas: List[str] = [f"S{abbr.value}" for abbr in self.power_gen_respondents]
 
     def _build_header(self, 
         frequency: Optional[str], 
@@ -20,9 +31,9 @@ class EIADataPuller:
         facets: Optional[Dict[str, list[str]]],
         start: str,
         end: str,
-        sort: Optional[List[Dict[str, str]]],
-        offset: int,
-        length: int
+        sort: Optional[List[Dict[str, str]]] = None,
+        offset: int = 0,
+        length: int = 1
     ) -> Dict[str, Any]:
         header = {
             "api_key": self.api_key,
@@ -50,39 +61,91 @@ class EIADataPuller:
         header["offset"] = 0
         header["length"] = MAX_QUERY_SIZE
         req = requests.get(url, headers=self._generate_header_str(header), params={"api_key": self.api_key})
+
+        print(req.json())
         return req.json()['response']['data'], int(req.json()['response']['total'])
     
-    def _get_res_data(self, header: Dict[str, Any], url: str, offset: int, length: int) -> List[Dict[str, Any]]:
+    def _get_data_with_offset(self, header: Dict[str, Any], url: str, offset: int, length: int) -> List[Dict[str, Any]]:
         header["offset"] = offset
         header["length"] = length
         req = requests.get(url, headers=self._generate_header_str(header), params={"api_key": self.api_key})
         return req.json()['response']['data']
 
-    def get_power_gen_data(self, fueltype: FuelType, respondent: Respondent):
-        POWER_GEN_START_DATE = "2019-01-01"
-        header_params = self._build_header(
-            frequency="daily",
-            data=["value"],
-            facets={"respondent": [respondent.value], "fueltype": [fueltype.value], "timezone": [SET_TIMEZONE.value]},
-            start=POWER_GEN_START_DATE,
-            end=datetime.now().strftime("%Y-%m-%d"),
-            sort=[
-                {
-                    "column": "period",
-                    "direction": "desc"
-                }
-            ],
-            offset=0,
-            length=1
-        )
-        power_gen_data_url = f"https://api.eia.gov/v2/electricity/rto/daily-fuel-type-data/data/"
-
-        res_list, res_size = self._get_data_and_size(header_params, power_gen_data_url)
-
+    def _get_all_data(self, header: Dict[str, Any], url: str) -> List[Dict[str, Any]]:
+        res_list, res_size = self._get_data_and_size(header, url)
         print(f"Querying {res_size} rows of data...")
         while len(res_list) < res_size:
             query_size = min(MAX_QUERY_SIZE, res_size - len(res_list))
-            res_list.extend(self._get_res_data(header_params, power_gen_data_url, len(res_list), query_size))
+            res_list.extend(self._get_data_with_offset(header, url, len(res_list), query_size))
             print(f"Recieved {len(res_list)} rows of data...")
-
         return res_list
+
+    def get_power_gen_data(self, fueltype: FuelType) -> pd.DataFrame:
+        """
+        Pulls EIA Power Generation Consumption data for a specific fueltype. 
+        Daily data is aggregated across all respondents for a given storage 
+        region, then is grouped again to find a weekly average.
+        """
+        POWER_GEN_URL = f"https://api.eia.gov/v2/electricity/rto/daily-fuel-type-data/data/"
+        POWER_GEN_START_DATE = "2019-01-01"
+        EXP_COLS = ["period", "value", "respondent-name"]
+        header = self._build_header(
+            frequency="daily",
+            data=["value"],
+            facets={"respondent": [respondent.value for respondent in self.power_gen_respondents], "fueltype": [fueltype.value], "timezone": [SET_TIMEZONE.value]},
+            start=POWER_GEN_START_DATE,
+            end=datetime.now().strftime("%Y-%m-%d"),
+        )
+
+        df = (pd.DataFrame(self._get_all_data(header, POWER_GEN_URL))[EXP_COLS]
+            .astype({"value": int, "period": "datetime64[ns]", "respondent-name": "category"})
+        )
+
+        df['Week'] = df['period'].dt.strftime("%U").astype(int)
+        df['Year'] = df['period'].dt.year
+        return (df.groupby(["period", "Year", "Week"])["value"].sum()
+            .reset_index(drop=False)
+            .groupby(["Week", "Year"])["value"].mean()
+            .reset_index(drop=False) 
+            .rename(columns={"value": f"Region_NG_Power_Gen_MWh"})
+        )
+
+
+
+    def get_storage_data(self) -> pd.DataFrame:
+        """
+        Pulls EIA Natural Gas Storage data for a specific storage
+        region. Data is retrieved at a weekly frequency.
+        """
+        STORAGE_URL = f"https://api.eia.gov/v2/natural-gas/stor/wkly/data/"
+        STORAGE_START_DATE= "2019-01-01"
+        EXP_COLS = ["period", "value"]
+
+        header = self._build_header(
+            frequency="weekly",
+            data=["value"],
+            facets={"series": [self.storage_region.value]},
+            start=STORAGE_START_DATE,
+            end=datetime.now().strftime("%Y-%m-%d"),
+        )
+
+        return (pd.DataFrame(self._get_all_data(header, STORAGE_URL))[EXP_COLS]
+            .astype({"value": int, "period": 'datetime64[ns]'})
+            .rename(columns={"value": f"{self.storage_region.name}_Region_NG_Storage_BCF"})
+            .sort_values(by="period", ascending=False)
+            .reset_index(drop=True)
+        )
+
+    def get_ng_usage_data(self, consumption_type: EIAConsumptionType) -> pd.DataFrame:
+        USAGE_URL = f"https://api.eia.gov/v2/natural-gas/cons/sum/data/"
+        USAGE_START_DATE = "2005-01-01"
+
+        header = self._build_header(
+            frequency="monthly",
+            data=["value"],
+            facets={"duoarea": self.eia_duoareas, "process": [consumption_type.value]},
+            start=USAGE_START_DATE,
+            end=datetime.now().strftime("%Y-%m-%d"),
+        )
+
+        return pd.DataFrame(self._get_all_data(header, USAGE_URL))
